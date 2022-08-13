22fd78be18372923c975bed0a98ed129734375c5
Helena Natanael <helena.natanael@tiket.com>
2022-08-13 15:13:21 +0700

    feat(final): final app using theCatAPI data

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# RISTEK Sisters in Tech 2022
+# RISTEK Sisters in Tech 2022: Catology
 
 © Helena Natanael, 2022
 
+Master branch contains starter-kit of this project, meanwhile Final branch contains the final look of this project.
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+I'm using [TheCatAPI](https://documenter.getpostman.com/view/5578104/RWgqUxxh#3cd6ebfd-338d-4a38-9929-f879a6bb0bd1) in this repo.
 
 ## Learn More
 
--- src/Routes/LandingPage/components/CatList.jsx
+++ src/Routes/LandingPage/components/CatList.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
 import getCats from '../../../Services/fetchApi';
+// import getCatsByName from '../../../Services/fetchApi';
+// import getCatsByAge from '../../../Services/fetchApi';
+
 import LoadingState from './LoadingState';
 import './catList.css';
 
 const View = () => {
+  const [selectedCatIdx, setSelectedCatIdx] = useState(null);
+
   // useQuery here
+  const {data: catListData, isLoading: isLoadingCatList} = useQuery('catList', getCats);
+  // const {data: catListByName, isLoading: isLoadingCatListByName} = useQuery('catListName', getCatsByName);
+  // const {data: catListByAge, isLoading: isLoadingCatListByAge} = useQuery('catListAge', getCatsByAge);
 
   // Loading state here
+  if(isLoadingCatList) {
+    return <LoadingState />
+  }
 
-  return <div className="padding">Hello Felines!</div>;
+  return (
+  <div className="padding">Hello Felines!
+    <div>
+      {
+        catListData.map((cat, idx) => (
+          <li key={idx} className="list-cat" onClick={() => setSelectedCatIdx(idx)}>
+            {cat.name}
+          </li>
+        ))
+      }
+    </div>
+    {
+      selectedCatIdx !== null && (
+        <img src={catListData[selectedCatIdx].image.url} alt={catListData[selectedCatIdx].name} width="200px" />
+      )
+    }
+  </div>
+  );
 };
 export default View;
